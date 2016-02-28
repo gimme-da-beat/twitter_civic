d42a78c323149647787cc6c388e151df51ac98b7
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-28 17:36:12 -0500

    working 2 apis

--- app/controllers/civics_controller.rb
+++ app/controllers/civics_controller.rb
@@ -5,6 +5,8 @@ class CivicsController < ApplicationController
     channels = []
     twitter_dirty = []
     twitter_refined = []
+    combined = []
+    state_from_google = ""
     @response["officials"].each do |qz|
       if qz["channels"] ==  nil
       elsif !qz["channels"].empty?
@@ -20,7 +22,25 @@ class CivicsController < ApplicationController
     twitter_dirty.each do |xxx|
       twitter_refined << xxx.values[1]
     end
-    render json:twitter_refined
+
+
+
+
+    @response["normalizedInput"].each do |xyz|
+    state_from_google << xyz.second if xyz.include?("state")
+    end
+
+    response2 = HTTParty.get("http://openstates.org/api/v1/legislators/?state=#{state_from_google}&chamber=upper&apikey=05f88e0536684aa6a5b9b7c923e4b3d8")
+
+    combined << response2
+    combined << twitter_refined
+
+
+
+
+
+
+    render json:combined
   end
 
   def show
