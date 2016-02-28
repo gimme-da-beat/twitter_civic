ce08a4680b0c93929c35b6e4135c37a84698ccbd
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-28 01:53:59 -0500

    twitter names compiled by address

--- app/controllers/civics_controller.rb
+++ app/controllers/civics_controller.rb
@@ -3,33 +3,27 @@ class CivicsController < ApplicationController
     address = params["address"]
     @response = HTTParty.get("https://www.googleapis.com/civicinfo/v2/representatives?address=#{address}&key=#{ENV["GOOGLE_KEY"]}")
     channels = []
-    @response["officials"].each do |q|
-      channels << q["channels"]
+    twitter_dirty = []
+    twitter_refined = []
+    @response["officials"].each do |qz|
+      if qz["channels"] ==  nil
+      elsif !qz["channels"].empty?
+          qz["channels"].each do |y|
+              y.each do |z|
+                if z.include?("Twitter")
+                twitter_dirty << y
+                end
+              end
+          end
       end
-      # byebug
-
-
-
-
-
-
-
-    render json:channels
-
+    end
+    twitter_dirty.each do |xxx|
+      twitter_refined << xxx.values[1]
+    end
+    render json:twitter_refined
   end
 
   def show
   end
-
-  # def channels
-  # responsex = @response["officials"]
-  # channels = []
-  # responsex.each do |q|
-  #   channels << responsex["channels"]
-  #   end
-  #   byebug
-  # end
-
-
 
 end
