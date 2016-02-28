d646ffcc3d77deb5aa9d7550c087149936596a5c
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-28 18:58:53 -0500

    spacing issues somewhat resolved

--- app/controllers/civics_controller.rb
+++ app/controllers/civics_controller.rb
@@ -26,32 +26,19 @@ class CivicsController < ApplicationController
       else twitter_refined << xxx.values[1]
       end
     end
-
     @response["normalizedInput"].each do |xyz|
     state_from_google << xyz.second if xyz.include?("state")
     end
-
     response2 = HTTParty.get("http://openstates.org/api/v1/legislators/?state=#{state_from_google}&chamber=upper&apikey=05f88e0536684aa6a5b9b7c923e4b3d8")
-
-
-
     response2.each do |zzz|
       zzz.each do |yyy|
       full_names << yyy.second if yyy.include?("full_name")
       photo_url << yyy.second if yyy.include?("photo_url")
       end
     end
- # byebug
-
     combined << full_names
     combined << photo_url
     combined << twitter_refined
-
-
-
-
-
-
     render json:combined
   end
 
