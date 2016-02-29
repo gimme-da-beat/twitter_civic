a687b1d1bc882ce2bae4c5ce54e2bd2c73e8a553
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-29 01:20:16 -0500

    update model/controller so everything is not in controller

--- app/controllers/civics_controller.rb
+++ app/controllers/civics_controller.rb
@@ -1,45 +1,11 @@
 class CivicsController < ApplicationController
   def index
     address = params["address"]
-    @response = HTTParty.get("https://www.googleapis.com/civicinfo/v2/representatives?address=#{address}&key=#{ENV["GOOGLE_KEY"]}")
-    channels = []
-    twitter_dirty = []
-    twitter_refined = []
-    combined = []
-    state_from_google = ""
-    full_names = []
-    photo_url = []
-    @response["officials"].each do |qz|
-      if qz["channels"] ==  nil
-      elsif !qz["channels"].empty?
-          qz["channels"].each do |y|
-              y.each do |z|
-                if z.include?("Twitter")
-                    twitter_dirty << y
-                end
-              end
-          end
-      end
-    end
-    twitter_dirty.each do |xxx|
-      if xxx.values[1] == "whitehouse"
-      else twitter_refined << xxx.values[1]
-      end
-    end
-    @response["normalizedInput"].each do |xyz|
-    state_from_google << xyz.second if xyz.include?("state")
-    end
-    response2 = HTTParty.get("http://openstates.org/api/v1/legislators/?state=#{state_from_google}&chamber=upper&apikey=05f88e0536684aa6a5b9b7c923e4b3d8")
-    response2.each do |zzz|
-      zzz.each do |yyy|
-      full_names << yyy.second if yyy.include?("full_name")
-      photo_url << yyy.second if yyy.include?("photo_url")
-      end
-    end
-    combined << full_names
-    combined << photo_url
-    combined << twitter_refined
-    render json:combined
+    feature_display = Civic.new(address)
+    feature_display.extract_twitter
+    feature_display.extract_state
+    feature_display.extract_full_names_and_photos
+    render json:feature_display.combiner
   end
 
   def show
--- app/models/civic.rb
+++ app/models/civic.rb
@@ -1,12 +1,57 @@
 class Civic < ActiveRecord::Base
+  def initialize(address)
+    @response = HTTParty.get("https://www.googleapis.com/civicinfo/v2/representatives?address=#{address}&key=#{ENV["GOOGLE_KEY"]}")
+    @combined = []
+    @state_from_google = ""
+    @full_names = []
+    @photo_url = []
+    @twitter_refined = []
+  end
 
-  def channels
-  responsex = @response["officials"]
-  channels = []
-  responsex.each do |q|
-    channels << responsex["channels"]
+  def extract_twitter
+    twitter_dirty = []
+    @response["officials"].each do |qz|
+      if qz["channels"] ==  nil
+      elsif !qz["channels"].empty?
+        qz["channels"].each do |y|
+          y.each do |z|
+            if z.include?("Twitter")
+              twitter_dirty << y
+            end
+          end
+        end
+      end
     end
-    byebug
+    twitter_dirty.each do |xxx|
+      if xxx.values[1] == "whitehouse"
+      else
+        @twitter_refined << xxx.values[1]
+      end
+    end
+  end
+
+  def extract_state
+    @response["normalizedInput"].each do |xyz|
+    @state_from_google << xyz.second if xyz.include?("state")
+    end
+  end
+
+
+
+  def extract_full_names_and_photos
+    leg_response = HTTParty.get("http://openstates.org/api/v1/legislators/?state=#{@state_from_google}&apikey=#{ENV["LEG_KEY"]}")
+    leg_response.each do |zzz|
+      zzz.each do |yyy|
+      @full_names << yyy.second if yyy.include?("full_name")
+      @photo_url << yyy.second if yyy.include?("photo_url")
+      end
+    end
+  end
+
+  def combiner
+    @combined << @twitter_refined
+    @combined << @full_names
+    @combined << @photo_url
   end
 
 end
